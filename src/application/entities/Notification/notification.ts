d2c96145d9ca2b3ee2ef1019cfe94aba8e654370
André Menezes Alberto de Souza <menezesbr@gmail.com>
2022-12-16 18:43:57 -0300

    Criando e persistindo notificações

--- src/application/entities/Notification/notification.ts
+++ src/application/entities/Notification/notification.ts
@@ -1,4 +1,5 @@
-import { Replace } from 'src/helpers/Replace';
+import { randomUUID } from 'node:crypto';
+import { Replace } from '../../../../src/helpers/Replace';
 import { Content } from './content';
 
 export interface NotificationProps {
@@ -10,13 +11,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id(): string {
+    return this._id;
   }
 
   public set recipientId(recipientId: string) {
